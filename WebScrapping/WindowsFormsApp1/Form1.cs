b3d438bd0300c1219d11bc28c47a0474bbff3a11
Luis Ricardo Celestino de Souza <souza.luis@gmail.com>
2019-09-08 11:12:33 -0300

    Initial version.

---
+++ WebScrapping/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AtualizarButton_Click(object sender, EventArgs e)
+        {
+            var wc = new WebClient();
+            string pagina = wc.DownloadString("https://social.msdn.microsoft.com/forums/pt-br/home");
+
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(pagina);
+
+            dataGridView1.Rows.Clear();
+
+            string id = string.Empty;
+            string titulo = string.Empty;
+            string postagem = string.Empty;
+            string exibicao = string.Empty;
+            string resposta = string.Empty;
+            string link = string.Empty;
+
+            foreach(HtmlNode node in htmlDocument.GetElementbyId("threadList").ChildNodes)
+            {
+                if (node.Attributes.Count > 0)
+                {
+                    id = node.Attributes["data-threadid"].Value;
+                    link = "https://social.msdn.microsoft.com/Forums/pt-BR/" + id;
+                    titulo = node.Descendants().First(x => x.Id.Equals("threadTitle_" + id)).InnerText;
+                    postagem = WebUtility.HtmlDecode( 
+                            node.Descendants().First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Equals("lastpost")).InnerText
+                                .Replace("\n", "").Replace("  ", "") 
+                        );
+                    exibicao = WebUtility.HtmlDecode(
+                            node.Descendants().First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Equals("viewcount")).InnerText
+                        );
+                    resposta = WebUtility.HtmlDecode(
+                            node.Descendants().First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Equals("replycount")).InnerText
+                        );
+
+                    if (!string.IsNullOrEmpty(titulo))
+                        dataGridView1.Rows.Add(titulo, postagem, exibicao, resposta, link);
+                }
+
+            }
+
+
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 4)
+                Process.Start(new ProcessStartInfo(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString()));
+        }
+    }
+}
